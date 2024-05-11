aa50a4d4639b654f4cb9eab77224cb24d1469929
Micah Rufsvold <86363075+mrufsvold@users.noreply.github.com>
2024-05-10 21:43:38 -0400

    satisfy aqua

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,19 +7,11 @@ using JET
 @run_package_tests
 
 @testset "InterfaceDispatch.jl" begin
-    # @testset "Code quality (Aqua.jl)" begin
-    #     Aqua.test_all(InterfaceDispatch; ambiguities = false,)
-    # end
+    @testset "Code quality (Aqua.jl)" begin
+        Aqua.test_all(InterfaceDispatch; ambiguities = false,)
+    end
     # @testset "Code linting (JET.jl)" begin
     #     JET.test_package(InterfaceDispatch; target_defined_modules = true)
     # end
     
-end
-
-module tmp
-    include("../src/InterfaceDispatch.jl")
-    
-end
-@macroexpand tmp.InterfaceDispatch.@interface struct HasEltype1{T}
-    function eltype(::tmp.InterfaceDispatch.This)::T where T end
 end
