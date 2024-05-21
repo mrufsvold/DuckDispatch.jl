d380db18b1e051d83094a5fb6c3bf856cc4fa094
Micah Rufsvold <mjrufsvold@mathematica-mpr.com>
2024-05-21 12:16:06 -0400

    clean up readability

--- src/Rewrite.jl
+++ src/Rewrite.jl
@@ -4,59 +4,45 @@ using ExproniconLite
 using BangBang: append!!
 
 struct This end
+
 struct Behavior{F, Sig<:Tuple} end
 
-function get_signature(::Type{Behavior{F,S}}) where {F,S}
-    return S
-end
-
-function get_func_type(::Type{Behavior{F,S}}) where {F,S}
-    return F
-end
+get_signature(::Type{Behavior{F,S}}) where {F,S} = S
+get_func_type(::Type{Behavior{F,S}}) where {F,S} = F
 
 abstract type DuckType{Behaviors} end
 
+narrow(::Type{T}, ::Any) where {T <: DuckType} = T
+
 function get_behaviors(::Type{D}) where D<:DuckType
     sup = supertype(D)
-    if sup === Any
-        return extract_behaviors(D)
-    end
+    sup === Any && return extract_behaviors(D)
     return get_behaviors(sup)
 end
-function extract_behaviors(::Type{DuckType{B}}) where {B}
-    return B
-end
+extract_behaviors(::Type{DuckType{B}}) where {B} = B
 
-function behaviors(::Type{D}) where D 
-    if D <: Behavior
-        return (D,)
-    end
+function behaviors_of(::Type{D}) where D 
+    D <: Behavior && (D,)
     these_behaviors = tuple_collect(get_behaviors(D))
-    return mapreduce(behaviors, append!!, these_behaviors)
+    return mapreduce(behaviors_of, append!!, these_behaviors)
 end
 function behaviors_union(::Type{D}) where D 
-    behavior_list = behaviors(D)
+    behavior_list = behaviors_of(D)
     return Union{behavior_list...}
 end
 
-extract_type(::Type{T}) where T = T
 @generated function tuple_collect(::Type{U}) where U
     types = tuple(Base.uniontypes(U)...)
     call = xcall(tuple, types...)
     return codegen_ast(call)
 end
 
-function narrow(::Type{T}, ::Any) where {T <: DuckType}
-    return T
-end
 
 struct Guise{DuckT, Data}
     data::Data
 end
 
-function get_duck_type(::Type{Guise{D, <:Any}}) where {D}
-    return D
-end
+get_duck_type(::Type{Guise{D, <:Any}}) where {D} = D
 
 """
     implies(::Type{DuckType}, ::Type{DuckType}})
@@ -64,16 +50,15 @@ Return true if the first DuckType is a composition that contains the second Duck
 """
 function implies(::Type{D1}, ::Type{D2}) where {D1<:DuckType, D2<:DuckType}
     # If we get an exact match between two DuckTypes, we can short-circuit to true
-    if D1 === D2
-        return true
-    end
+    D1 === D2 && return true
+
     # We can also check all the behaviors of D2 and see if they are in the union of
     # behaviors for D1. First, we try to just use the type system to check if D2 <: D1
     behaviors_union(D2) <: behaviors_union(D1) && return true
     
     # If that fails, we need to check the behaviors of D2 one by one
-    d1_behaviors = behaviors(D1)
-    for b2 in behaviors(D2)
+    d1_behaviors = behaviors_of(D1)
+    for b2 in behaviors_of(D2)
         any(implies(b1, b2) for b1 in d1_behaviors) || return false
     end
     return true
@@ -95,7 +80,7 @@ end
 
 @generated function quacks_like(::Type{Duck}, ::Type{Data}) where {Duck<:DuckType, Data}
     type_checker = TypeChecker{Data}(Data)
-    behavior_list = behaviors(Duck)
+    behavior_list = behaviors_of(Duck)
     check_quotes = Expr[
         :($type_checker($b) || return false)
         for b in behavior_list
@@ -103,22 +88,23 @@ end
     return Expr(:block, check_quotes..., :(return true))
 end
 
-
-
 function wrap(::Type{Duck}, data::T) where {Duck<:DuckType, T}
     NarrowDuckType = narrow(Duck, T)::Type{<:Duck}
     quacks_like(NarrowDuckType, T) && return Guise{NarrowDuckType,T}(data)
     error("Type $T does not implement the methods required by $NarrowDuckType")
 end
+unwrap(x::Guise) = x.data
+rewrap(x::Guise{I1, <:Any}, ::Type{I2}) where {I1, I2<:DuckType} = wrap(I2, unwrap(x))
 
-function unwrap(x::Guise)
-    return x.data
+
+@generated function dispatch_required_method(beh::Type{Behavior{F, S}}, args...; kwargs...) where {F, S}
+    DuckT = some_function_to_get_the_duck_type(beh, args)
+    if isnothing(target_duck_type)
+        error("No fitting iterate method found for $Duck")
+    end
+    ret_type = get_return_type(DuckT, beh)
+    return ($(F.instance)(unwrap_where_this($S, $args)...;$kwargs...)::$ret_type)
 end
-
-function rewrap(x::Guise{I1, <:Any}, ::Type{I2}) where {I1, I2<:DuckType}
-    return wrap(I2, unwrap(x))
-end
-
 
 # an example
 struct Iterable{T} <: DuckType{
@@ -143,13 +129,7 @@ function Base.iterate(arg1::Guise{Duck, <:Any}, arg2) where Duck
     return dispatch_required_method(beh, Duck, arg1, arg2)
 end
 
-function dispatch_required_method(beh::Type{Behavior{F, S}}, ::Type{Duck}, args...) where {F, S, Duck}
-    target_duck_type = check_for_fitting_duck_type(Duck, beh)
-    if isnothing(target_duck_type)
-        error("No fitting iterate method found for $Duck")
-    end
-    return F.instance(unwrap_where_this(S, args)...)::get_return_type(target_duck_type, beh)
-end
+
 
 function unwrap_where_this(sig::Type{<:Tuple}, args::Tuple)
     return map(sig, args) do (T, arg)
@@ -184,24 +164,4 @@ using Test
     @test wrap(IsContainer{Int}, [1,2,3]) isa Guise{IsContainer{Int}, Vector{Int}}
     @test rewrap(wrap(IsContainer{Int}, [1,2,3]), Iterable) isa Guise{Iterable{Int}, Vector{Int}}
 end
-end
-
-
-module mwe
-abstract type Abstract{T} end
-struct StaticConcrete
-struct Concrete{A,B} <: Abstract{Abstract{B}} end
-
-function get_T(::Type{X}) where X <: Abstract
-    sup = supertype(X)
-    if sup === Any
-        return extract_T(X)
-    end
-    return get_T(sup)
-end
-function extract_T(::Type{Abstract{T}}) where T
-    return T
-end
-
-@show get_T(Concrete)
 end
